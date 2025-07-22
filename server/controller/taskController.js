a159476e610fcfc515031c52cfc0f4a6d1acd7ad
Shivankkumar09 <shivank0912@gmail.com>
2025-07-22 20:43:36 +0530

    fixed date

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -1,16 +1,23 @@
 const Task = require('../db/taskSchema');
 
- const addTask = async (req, res) => {
-     try {
+const addTask = async (req, res) => {
+  try {
     const { title, desc, tags } = req.body;
     const tagArray = typeof tags === 'string' ? tags.split(',').map(t => t.trim()) : tags;
+
     const newTask = new Task({ title, desc, tags: tagArray });
     const savedTask = await newTask.save();
-    res.status(201).json(savedTask);
+
+    const formattedTask = {
+      ...savedTask._doc,
+      createdAt: savedTask.createdAt.toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })
+    };
+
+    res.status(201).json(formattedTask);
   } catch (err) {
     res.status(500).json({ error: 'Failed to create task' });
   }
-}
+};
 
  const getTasks = async (req, res) => {
     try {
